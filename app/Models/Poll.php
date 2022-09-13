28f5d728a239965e61c06db9e424d1bc90833416
MAQ-97 <51742322+MAQ-97@users.noreply.github.com>
2022-09-13 18:46:17 +0500

    Update Poll.php

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Poll extends Model
 {
-    protected $appends = ['liked_users','user','exist_users','marked_option','progress','total_record'];
+    protected $appends = ['liked_users', 'user', 'exist_users', 'marked_option', 'progress', 'total_record'];
 
     public function comments()
     {
@@ -40,31 +40,31 @@ class Poll extends Model
 
     public function getProgressAttribute()
     {
-        $user_option=count($this->user_option()->get());
-        $audience=$this->audience;
-        $percentage=round(($user_option/$audience)*100);
+        $user_option = count($this->user_option()->get());
+        $audience = $this->audience;
+        $percentage = round(($user_option / $audience) * 100);
         return $percentage;
     }
 
     public function getTotalRecordAttribute()
-	{
-		return $this->count();
-	}
+    {
+        return $this->count();
+    }
 
     public function getUserAttribute()
     {
-        $user_id=$this->user_id;
+        $user_id = $this->user_id;
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://rrci.staging.rarare.com/user/'.$user_id,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_URL => 'https://rrci.staging.rarare.com/user/' . $user_id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
         ));
 
         $response = curl_exec($curl);
@@ -75,12 +75,29 @@ class Poll extends Model
 
     public function getMarkedOptionAttribute()
     {
-        $option_array=array();
-        $option=$this->options;
-        foreach($option as $item){
-            $option_array[$item->id]=count($item->user_option);
+        $option_array = array();
+        $option = $this->options;
+        foreach ($option as $item) {
+            $option_array[$item->id] = count($item->user_option);
         }
 
         return $option_array;
+    }
+
+    public function getStatusAttribute()
+    {
+        $start = gmdate('Y-m-d H:i:s', strtotime("$this->start_date $this->start_time"));
+        $end = gmdate('Y-m-d H:i:s', strtotime("$this->end_date $this->end_time"));
+        $now = date("Y-m-d H:i:s");
+
+        if ($start > $now && $end < $now) {
+            return 1;
+        }
+
+        if ($start < $now) {
+            return 0;
+        }
+
+        return 2;
     }
 }
