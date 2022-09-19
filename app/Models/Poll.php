ffd403d99b1963ecc1c8f1207c4bb693e73d6344
ahsanmirza10 <mirzaahsan465@gmail.com>
2022-09-19 20:07:41 +0500

    status change

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -102,13 +102,13 @@ class Poll extends Model
 	    $end=$end_dt->format('Y-m-d h:i A');
 
 	    if($start < $now && $end > $now){
-	    	return 1;
+	    	return "In Progress";
 	    }
 
 	    if($start > $now){
-	    	return 0;
+	    	return "Pending";
 	    }
 
-	    return 2;
+	    return "Completed";
     }
 }
